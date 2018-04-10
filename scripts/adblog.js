c486486ee0c87fcc0f0adda660dbe98458132a99
ocknamo <s14pes@gmail.com>
2018-04-10 16:46:46 +0900

    ツイート間隔を３時間に変更

--- scripts/adblog.js
+++ scripts/adblog.js
@@ -7,7 +7,8 @@
 'use strict';
 const urls = [
 	'おっぱい',
-	'おっぱお'
+  'おっぱお',
+  'https://www.youtube.com/watch?v=llI1rGWLjc8'
 ]
 // 定期処理するオブジェクトを宣言
 var CronJob = require('cron').CronJob;
@@ -17,9 +18,8 @@ function url(){
 }
 module.exports = (robot) => {
 // 定期実行時に呼ばれるメソッド
-//var job = new CronJob('0 0 0,3,6,9,12,15,18,21 * * *', function() {
-var job = new CronJob('0 * * * * *', function() {
-    robot.send('ocknamo_tw_bot', 'BOTちゃん' + url());
+var job = new CronJob('0 0 0,3,6,9,12,15,18,21 * * *', function() {
+    robot.send('ocknamo_tw_bot', 'BOTちゃん「 ' + url() + '」');
   }, null, true, 'Asia/Tokyo');
   job.start();
 };
